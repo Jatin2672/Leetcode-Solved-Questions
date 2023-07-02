628640faf52fa4e6050eec10cc933a207d75e5b7
JATIN SINGH <jatinsingh2672@gmail.com>
2023-07-03 00:44:31 +0530

    Added solution - LeetHub

--- Search in Linked List - GFG/search-in-linked-list.cpp
+++ Search in Linked List - GFG/search-in-linked-list.cpp
@@ -27,14 +27,14 @@ class Solution {
   public:
     // Function to count nodes of a linked list.
     bool searchKey(int n, struct Node* head, int key) {
-        // Code here
-        while(head != NULL){
-            if(head->data == key){
-                return true;
-            }
-            head = head->next;
-        }
-        return false;
+       Node *temp=head;
+       while(temp!=NULL){
+           if(temp->data==key){
+               return true;
+           }
+           temp=temp->next;
+       }
+       return false;
     }
 };
 
